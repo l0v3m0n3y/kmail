0a8117107af113bb67aa46017ce8a816027e6b30
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-01-19 12:51:41 +0300

    Update kmail.swift

--- src/kmail.swift
+++ src/kmail.swift
@@ -2,6 +2,25 @@ import Foundation
 #if canImport(FoundationNetworking)
 import FoundationNetworking
 #endif
+
+extension URLSession {
+    func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+        return try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                } else if let data = data, let response = response {
+                    continuation.resume(returning: (data, response))
+                } else {
+                    continuation.resume(throwing: URLError(.unknown))
+                }
+            }
+            task.resume()
+        }
+    }
+}
+
+
 public class Kmail {
     private let api = "https://kmail.pw"
     private var headers: [String: String]
